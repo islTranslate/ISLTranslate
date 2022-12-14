824bade858696496a85f4f4836087dde44f6c573
islTranslate <118555235+islTranslate@users.noreply.github.com>
2022-12-14 18:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-# Import required libraries
 
 import cv2
 import numpy as np
 import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
+import requests
+import os
+
+# Home UI 
 
 def main():
+
     st.set_page_config(layout="wide")
+
     font_css = """
         <style>
         button[data-baseweb="tab"] {
@@ -19,45 +20,73 @@ def main():
         """
 
     st.write(font_css, unsafe_allow_html=True)
+
+    tabs = st.tabs(('Panorama Image'))
+
+    with tabs[0]:
+        panorama()
+
+# Pre-process Image
+def preProcessImg(img):
+    # Pre-processing image: resize image
+    height, width, _ = img.shape
+    width = int(720/height*width)
+    height = 720
+    img = cv2.resize(img,(width,height))
+    return img
+
+# Upload Image
+def uploadImages(key):
+
+    uploaded_files = st.file_uploader("Choose Image files in Proper sequence",key=key,accept_multiple_files=True)
+    imgs = list()
+    for file in uploaded_files:
+        file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+        # Pre-processing image: resize image
+        imgs.append(preProcessImg(img))
     
-    playVid()
-
-def playVid():
-
+    if imgs:
+        return imgs
     
-    stt_button = Button(label="Speak", width=100)
+    img = cv2.cvtColor(preProcessImg(cv2.imread('sample.jpg')),cv2.COLOR_BGR2RGB)
+    size = img.shape[1] - 1
+    return [
+        img[:,:int(1/2*size),:],
+        img[:,int(1/4*size):int(3/4*size),:],
+        img[:,int(1/2*size):,:]
+    ]
+# Panorama 
 
-    stt_button.js_on_event("button_click", CustomJS(code="""
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
- 
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-            }
-        }
-        recognition.start();
-        """))
+def panorama():
 
-    result = streamlit_bokeh_events(
-        stt_button,
-        events="GET_TEXT",
-        key="listen",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0)
+    st.header("Panorama Image")
 
-    if result:
-        if "GET_TEXT" in result:
-            st.write(result.get("GET_TEXT"))
+    imgs = uploadImages(0)
 
+    # Original Image
+    st.subheader("Original Images")
+
+    original_imgs = st.columns(len(imgs))
+    for (original_img,img) in zip(original_imgs,imgs):
+        with original_img:
+            st.image(img,use_column_width=True)
+
+      
+    st.subheader("Panorama Image")
+
+    stitchy = cv2.Stitcher.create()
+    ok,panorama =stitchy.stitch(imgs)
+
+    if ok != cv2.STITCHER_OK:
+    # checking if the stitching procedure is successful
+    # .stitch() function returns a true value if stitching is
+    # done successfully
+        st.warning("Panorama is not possible for given images in given sequence!!!")
+    else:
+        st.image(panorama,use_column_width=True)
 
 if __name__ == "__main__":
     main()
